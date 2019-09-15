5616e53d13944c0217ff0539ee2f28ddff73528a
Frida <fridasoso@gmail.com>
2019-09-15 18:45:28 +0200

    Closes #1, #2

--- src/App.js
+++ src/App.js
@@ -1,46 +1,98 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import kiwi from './kiwi.svg';
 import './App.css';
+import Header from './Components/Header'
+import Checkbox from './Components/Checkbox'
+import Checkboxes from './Components/Checkboxes'
 
 
-function App() {
-  return (
+
+class App extends Component {
+
+    state = {
+      checkboxes: [
+        {
+          id: 1,
+          title: 'Elephant',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Giraffe',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Zebra',
+          completed: false
+        },
+        {
+          id: 4,
+          title: 'Nature',
+          completed: false
+        },
+        {
+          id: 5,
+          title: 'Jazz',
+          completed: false
+        },
+        {
+          id: 6,
+          title: 'Telephone',
+          completed: false
+        },
+        {
+          id: 7,
+          title: 'Lyrics',
+          completed: false
+        },
+        {
+          id: 8,
+          title: 'Novel',
+          completed: false
+        },
+        {
+          id: 9,
+          title: 'Poem',
+          completed: false
+        }
+      ]
+    };
+  
+
+  // Toggle checked
+  checked = (id) => {
+    this.setState({
+      checkboxes: this.state.checkboxes.map((checkbox) => {
+        if (checkbox.id === id){
+          checkbox.completed = !checkbox.completed
+          console.log(id)
+        }
+        return checkbox;
+      })
+    });
+  };
+  
+render(){  
+  return(
     <div className="App">
-      <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <h1>
-          Galleri
-        </h1>
-        <div className="Frame">
-        <img src={kiwi} className="kiwi-bird" alt="kiwi" />
-        </div>
-       
-
-        {/*<a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello world
-        </a>*/}
-      </header>
-      <form>
-          <label>
-          Tema:
-          <input
-            name="isGoing"
-            type="checkbox"
-             />
-        </label>
-        </form>
-    
-      
+      <Header />
+        <div className="container1">
+          <div className="Content">
+            <div className='image1'><img src={kiwi} className="kiwi-bird" alt="kiwi" /></div>
+            <div className='text1'><h5>Hællæ</h5></div>
+          </div>
+          <Checkboxes checkboxes={this.state.checkboxes} checked={this.checked}/>
+        </div>     
     </div>
   );
-}
+}};
 
+/*const checkboxesStyle = {
+  display: flex,
+  direction: column,
+}*/
 
 
 export default App;
