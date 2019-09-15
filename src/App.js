eaff165294b98249fa8bc411cb912976ac72df61
sunnivbl <sunnivbl@stud.ntnu.no>
2019-09-15 13:40:27 +0200

    Lagt til bilde

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
+import kiwi from './kiwi.svg';
 import './App.css';
+
 
 function App() {
   return (
@@ -10,7 +12,11 @@ function App() {
         <h1>
           Galleri
         </h1>
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="Frame">
+        <img src={kiwi} className="kiwi-bird" alt="kiwi" />
+        </div>
+       
+
         {/*<a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +26,17 @@ function App() {
           Hello world
         </a>*/}
       </header>
+      <form>
+          <label>
+          Tema:
+          <input
+            name="isGoing"
+            type="checkbox"
+             />
+        </label>
+        </form>
+    
+      
     </div>
   );
 }
