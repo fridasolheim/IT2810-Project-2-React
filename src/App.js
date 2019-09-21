feef59acfdbeb001fbfd0d73fcb020516199907a
Frida <fridasoso@gmail.com>
2019-09-21 17:23:06 +0200

    Added functionality and buttons for local storage. Choices are now pushed and can be fetched from LS

--- src/App.js
+++ src/App.js
@@ -16,11 +16,10 @@ class App extends Component {
     this.state = {
       chosenMedia:[],
       chosenPicture:1,
-      chosenText:7,
       chosenSound:4,
-
+      chosenText:7,
+      
       animals: [
-        
         {
           id: 1,
           title: 'Elephant',
@@ -77,13 +76,9 @@ class App extends Component {
         return false;
      }
   });*/
-    
+}
 
- 
-
-  }
-
-     // Toggle checked
+  // Toggle checked
   checked = (id) => {
     this.setState({
       animals: this.state.animals.map((checkbox) => {
@@ -113,70 +108,79 @@ class App extends Component {
       })
     });
   };
-    
-  
 
-  
+  //NB! handleClick/generate art must have been clicked before trying to save and fetch the numbers.
+  handleSave = () => {
+    const {chosenPicture, chosenSound, chosenText} = this.state;
+    localStorage.setItem('chosenPicture', this.state.chosenPicture);
+    localStorage.setItem('chosenSound', this.state.chosenSound);
+    localStorage.setItem('chosenText', this.state.chosenText);
+    console.log("Numbers transmitted to local storage");
+  };
 
+  handleFetch = () => {
+    const chosenPicture = localStorage.getItem('chosenPicture');
+    const chosenSound = localStorage.getItem('chosenSound');
+    const chosenText = localStorage.getItem('chosenText');
+    this.setState({chosenPicture, chosenSound, chosenText});
+    console.log("Fetched numbers: ", chosenPicture, chosenSound, chosenText)
+  };
+  
+  //upon clicking generate art
   handleClick () {
-    
-    
     for (var element in this.state.chosenMedia){
       if (this.state.chosenMedia[element]<4){
-        this.state.chosenPicture=this.state.chosenMedia[element];
+        this.setState({chosenPicture: this.state.chosenMedia[element]});
       }
       if (this.state.chosenMedia[element]>3 && this.state.chosenMedia[element]<7){
-        this.state.chosenSound=this.state.chosenMedia[element];
+        this.setState({chosenSound: this.state.chosenMedia[element]});
       }
       if (6<this.state.chosenMedia[element]){
-        this.state.chosenText=this.state.chosenMedia[element];
+        this.setState({chosenText: this.state.chosenMedia[element]});
       }
-    }
-    
+    }  
     console.log("Pic:"+this.state.chosenPicture,",Sound:"+this.state.chosenSound,",Text:"+this.state.chosenText)
   }
 
 render(){  
-
   return(
-    
     <div className="App">
-      
       <Header/>
       <div className="container1">
-      <Tabs>
-        
-      <div label="Welcome">
-        <div className="Content">
+        <Tabs>
+        <div label="Welcome">
+          <div className="Content">
             <div className='image1'><img src={kiwi} className="kiwi-bird" alt="kiwi" /></div>
-            <div className='text1'><h5>Hællæ</h5></div>
-            
+            <div className='text1'><h5>Hællæ</h5></div> 
+            </div>
         </div>
-      </div>
-      <div label="Page 1">
-      <Page soundNr={this.state.chosenSound}/>
-      </div>
-      <div label="Page 2">
-        See ya later, <em>Alligator</em>!
-      </div>
-      <div label="Page 3">
-        After 'while, <em>Crocodile</em>!
-      </div>
-      <div label="Page 4">
-        Heihei
-      </div>
-      </Tabs>
-      <div className= "checkboxesStyle">
-        <h2>PICTURE</h2>
-        <Checkboxes checkboxes={this.state.animals} checked={this.checked}/>
-        <h2>SOUNDS</h2>
-        <Checkboxes checkboxes={this.state.sounds} checked={this.checked}/>
-        <h2>TEXT</h2>
-        <Checkboxes checkboxes={this.state.text} checked={this.checked}/>
-        <div onClick={this.handleClick} className="generate">GENERATE ART</div>
-      </div>
-    </div> 
-          
+        <div label="Page 1">
+          <Page soundNr={this.state.chosenSound}/>
+        </div>
+        <div label="Page 2">
+          See ya later, <em>Alligator</em>!
+        </div>
+        <div label="Page 3">
+          After 'while, <em>Crocodile</em>!
+        </div>
+        <div label="Page 4">
+          Heihei
+        </div>
+        </Tabs> 
+        <div className= "checkboxesStyle">
+          <h2>PICTURE</h2>
+          <Checkboxes checkboxes={this.state.animals} checked={this.checked}/>
+          <h2>SOUNDS</h2>
+          <Checkboxes checkboxes={this.state.sounds} checked={this.checked}/>
+          <h2>TEXT</h2>
+          <Checkboxes checkboxes={this.state.text} checked={this.checked}/>
+        </div>
+      </div>    
+      <div className='container2'>
+        <div onClick={this.handleSave} className="buttonStyle">SAVE ART</div>
+        <div onClick={this.handleFetch} className="buttonStyle">FETCH ART</div>
+        <div onClick={this.handleClick} className="buttonStyle">GENERATE ART</div>
+      </div>     
     </div>
   );
 }};
