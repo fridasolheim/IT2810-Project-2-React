da543a82c55ae1b6a01387c0bc9a0f15a37251de
Frida <fridasoso@gmail.com>
2019-09-22 13:51:46 +0200

    hei 2.0

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 function Header(){
     return(
-
         <div className='Header'>
             <h1 className="h1-header">WELCOME </h1>
             <h5 className="h5-header">TO OUR RANDOM ART GENERATOR</h5>
         </div>
-
     )
 }
 
