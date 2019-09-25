73a5def75c8bc991f0e24b0e20aee9db3010bcb9
sunnivbl <sunnivbl@stud.ntnu.no>
2019-09-25 19:18:13 +0200

    prøvd å få til ny build, endret litt på bilder

--- src/App.js
+++ src/App.js
@@ -129,9 +129,7 @@ render(){
             <p>2. Press "Generate art"</p><p>3. Click on the tabs to see your collection</p><p>4. Click "Save art" to save the collection</p></div> 
           </div>
         </div>
-        <div label="0">
-          <Page soundNr={this.state.chosenSound} pictureNr={this.state.chosenPicture} textNr={this.state.chosenText} activeTab={this.state.activeTab}/>
-        </div>
+        
         <div label="1">
           <Page soundNr={this.state.chosenSound} pictureNr={this.state.chosenPicture} textNr={this.state.chosenText} activeTab={this.state.activeTab}/>
           
