2bbdd72ee14eb7ea2934a0785f055b0060a715db
sunnivbl <sunnivbl@stud.ntnu.no>
2019-09-26 13:04:51 +0200

    prøver igjen

--- src/Components/Page.js
+++ src/Components/Page.js
@@ -88,13 +88,13 @@ class Page extends Component {
 
     displaySound(){
         if (this.soundNumber===4){
-            this.state.playlist.push("./Tecno1.mp3","./Tecno2.mp3","./Tecno3.mp3","./Tecno4.mp3")
+            this.state.playlist.push("/Tecno1.mp3","/Tecno2.mp3","/Tecno3.mp3","/Tecno4.mp3")
         }
         if (this.soundNumber===5){
-            this.state.playlist.push("./Jazz1.mp3","./Jazz2.mp3","./Jazz3.mp3","./Jazz4.mp3")
+            this.state.playlist.push("/Jazz1.mp3","/Jazz2.mp3","/Jazz3.mp3","/Jazz4.mp3")
         }
         if (this.soundNumber===6){
-            this.state.playlist.push("./Ringtone1.mp3","./Ringtone2.mp3","./Ringtone3.mp3","./Ringtone4.mp3")
+            this.state.playlist.push("/Ringtone1.mp3","/Ringtone2.mp3","/Ringtone3.mp3","/Ringtone4.mp3")
         }
     }
 
